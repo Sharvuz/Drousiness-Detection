e0e1b795c846750f8b0f62cbfa6f8eb88771ab4c
Huynh Quang Vu <vu.hqng@gmail.com>
2026-05-11 23:37:19 +0700

    Update rec_system.py

--- src/rec_system.py
+++ src/rec_system.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 def recommend(gdf, emotion):
-  for entry in gdf:
-    if emotion=='happy' or emotion=='drowsy':
-      return gdf.get_group(('high', 'high'))
-    elif emotion=='neutral':
-      return gdf.get_group(('high', 'low'))
-    elif emotion=="angry" or emotion=="disgust" or emotion=="surprised":
-      grp1 = gdf.get_group(('low', 'high'))
-      grp2 = gdf.get_group(('high', 'low'))
-      return grp1.append(grp2)
-    elif emotion=="sad" or emotion=="scared":
-      grp1 = gdf.get_group(('low', 'low'))
-      grp2 = gdf.get_group(('high', 'high'))
-      return grp1.append(grp2)
-
+    # Trả về các nhóm bài hát dựa theo cảm xúc
+    if emotion == 'happy' or emotion == 'drowsy':
+        return gdf.get_group(('high', 'high'))
+    elif emotion == 'neutral':
+        return gdf.get_group(('high', 'low'))
+    elif emotion in ["angry", "disgust", "surprised"]:
+        grp1 = gdf.get_group(('low', 'high'))
+        grp2 = gdf.get_group(('high', 'low'))
+        return pd.concat([grp1, grp2]) # Dùng concat thay vì append
+    elif emotion in ["sad", "scared"]:
+        grp1 = gdf.get_group(('low', 'low'))
+        grp2 = gdf.get_group(('high', 'high'))
+        return pd.concat([grp1, grp2])
+    return None
